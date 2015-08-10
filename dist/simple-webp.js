06a603da04273055756b184370fca9f71e4fe155
ymatuhin <ymatuhin@yandex.ru>
2015-08-10 14:58:36 +0300

    [add] readme and new version

--- dist/simple-webp.js
+++ dist/simple-webp.js
@@ -1,21 +1,20 @@
-(function () {
-	var html = document.documentElement,
-		isSupported = null;
+(function (window) {
+	var html = document.documentElement, isSupported = null;
 
 	// dom ready fn
-	function ready(fn) {
+	function domReady(fn) {
 		if (document.readyState !== 'loading') fn();
 		else document.addEventListener('DOMContentLoaded', fn);
 	}
 
-	function eachNoWebpImages (action) {
-		var action = action || function () {};
-		var images = document.querySelectorAll('img[data-webp]:not([src$=webp])');
+	function each (action) {
+		action = action || function () {};
+		var elements = document.querySelectorAll('noscript[data-webp]');
 
-		if (images.length === 0) return;
+		if (elements.length === 0) return;
 
-		for (var i = 0; i < images.length; i++) {
-			var el = images[i];
+		for (var i = 0; i < elements.length; i++) {
+			var el = elements[i];
 			action(el);
 		}
 	}
@@ -29,28 +28,60 @@
 				if (html.className.indexOf('no-webp') >= 0)
 					html.className = html.className.replace(/\bno-webp\b/, 'webp');
 				else html.className += ' webp';
-				fn();
 			}
+			fn(isSupported);
 		};
 		WebP.src = 'data:image/webp;base64,UklGRjoAAABXRUJQVlA4IC4AAACyAgCdASoCAAIALmk0mk0iIiIiIgBoSygABc6WWgAA/veff/0PP8bA//LwYAAA';
 	}
 
-	function changeToWebpUrl () {
-		eachNoWebpImages(function (el) {
-			if (el.complete) return;
-			el.src = el.src.replace(/\.\D{3,5}$/g, '.webp');
+	function addDefault () {
+		each(function (el) {
+			el.insertAdjacentHTML('afterend', el.textContent);
+			el.parentNode.removeChild(el);
 		});
 	}
 
-	function webpSupportFnCallback () {
-		changeToWebpUrl();
-		var timer = setInterval(changeToWebpUrl, 50);
+	function addWebP () {
+		each(function (el) {
+			var str = el.textContent;
+			var p1 = str.indexOf(' src=');
+			var p2 = str.indexOf(' ', p1 + 1);
+			var p3 = str.indexOf('>', p1 + 1);
+			var pLast = (p2 > p3) ? p3 : p2;
+
+			var src = str.slice(p1, pLast);
+			var pLastDot = src.lastIndexOf('.');
+
+			var extension = src.slice(pLastDot);
+			var newExtension = extension.replace(/\.[a-zA-Z]{3,5}/g, '.webp');
+
+			var newStr = str.slice(0, p1 + pLastDot) + newExtension + str.slice(pLast);
+
+			el.insertAdjacentHTML('afterend', newStr);
+			el.parentNode.removeChild(el);
+		});
+	}
+
+	function interV(support) {
+		if (support) addWebP();
+		else addDefault();
+	}
+
+	function webpSupportFnCallback (support) {
+		var timer;
+
+		interV(support);
+		timer = setInterval(interV.bind(null, support), 15);
 
 		// dom ready
-		ready(function () {
+		domReady(function () {
 			clearInterval(timer);
 		});
+
+		return 'done'
 	}
 
 	checkSupport(webpSupportFnCallback);
-})();
+
+	window.simpleWebp = webpSupportFnCallback.bind(null, isSupported);
+})(window);
---
+++ readme.md
@@ -0,0 +1,10 @@
+# Build
+`gulp`
+
+## Comit new version
+
+```
+git commit -m "Add bower.json."
+$ git tag "v0.0.1"
+$ git push origin --tags
+```
